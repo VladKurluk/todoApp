e9248bb54b1c0d76068b5e1c0a6179259be67b6d
Vlad Kurluk <vlad11kurluk89@gmail,com>
2018-04-01 20:06:14 +0300

    Develop create DOM-element and other method to change listItem

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 /*
-* 4) Описываю функцию, которая создает html-элементы
+* 4) Описываю функцию, которая создает DOM-элементы
 */
 function createTodoItem(title) {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
 
     const label = document.createElement('label');
     label.innerText = title;
@@ -11,15 +12,15 @@ function createTodoItem(title) {
 
     const editInput = document.createElement('input');
     editInput.type = 'text';
-    editInput.clasName = 'textfield';
+    editInput.className = 'textfield';
 
     const editButton = document.createElement('button');
     editButton.innerText = 'Изменить';
-    editButton.clasName = 'edit';
+    editButton.className = 'edit';
 
     const deleteButton = document.createElement('button');
     deleteButton.innerText = 'Удалить';
-    deleteButton.clasName = 'delete';
+    deleteButton.className = 'delete';
 
     const listItem = document.createElement('li');
     listItem.className = 'todo-item';
@@ -29,19 +30,74 @@ function createTodoItem(title) {
     listItem.appendChild(editInput);
     listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
-    console.log(listItem);
+
+    //Подписка на события
+    bindEvents(listItem);
+
     return listItem;
 }
 
 /*
-* 3) Описываю функцию обработчик события
+* 5) Описываю ф-ию привязки событий на кнопки задачи в списке
+*/
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('.checkbox');
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+    checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
+}
+
+/*
+* 3) Описываю функцию обработчик события,
+*    которая добавляет задачу в список
 */
 function addTodoItem(event) {
     event.preventDefault();
 
     if (addInput.value === "") return alert('Ты не ввел название задачи!');
 
-    const listItem = createTodoItem(addInput.value);
+    const todoItem = createTodoItem(addInput.value);
+    todoList.appendChild(todoItem);
+    //Очистка поля формы после добавления задачи в список
+    addInput.value = '';
+}
+
+/*
+* 6) Описываю ф-ию для события на Чекбоксе
+*/
+function toggleTodoItem() {
+    const listItem = this.parentNode;
+    listItem.classList.toggle('completed');
+}
+
+/*
+* 7) Описываю ф-ию для события на кнопке Изменить
+*/
+function editTodoItem() {
+    const listItem = this.parentNode;
+    const title = listItem.querySelector('.title');
+    const editInput = listItem.querySelector('.textfield');
+    const isEditing = listItem.classList.contains('editing');
+
+    if (isEditing) {
+        title.innerText = editInput.value;
+        this.innerText = 'Изменить';
+    } else {
+        editInput.value = title.innerText;
+        this.innerText = 'Сохранить';
+    }
+
+    listItem.classList.toggle('editing');
+}
+
+/*
+* 8) Описываю ф-ию для события 
+*/
+function deleteTodoItem() {
+
 }
 
 /*
@@ -56,3 +112,5 @@ const todoItems = document.querySelectorAll('.todo-item');
 * 2) Привязую обработчик события на отправку формы
 */
 todoForm.addEventListener('submit', addTodoItem);
+
+
